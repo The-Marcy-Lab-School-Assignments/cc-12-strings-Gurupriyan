f4aab83fff4d00b3065a3adde376688251f54676
Gurupriyan <gurupriyan98@gmail.com>
2021-11-03 14:44:25 +0000

    Challenge completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //write your code here
+// -declare a function named "reverseString()' with a parameter that accepts a string argument
+// -make a empty string variable
+// -iterate through the argument with a for loop starting from last and going back to the first 
+// - for each i add it to the empty string variable
+// - return the empty string(which is not empty after the for loop)
+function reverseString(string){
+    let reverse = "";
+    for(let i=string.length-1; i>=0; i--){
+        reverse += string[i];
+    }
+    return reverse;
+}
+
+console.log(reverseString("hello"));
